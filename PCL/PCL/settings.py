5776a2c00f3af39d7201e42b0165cda755a803b4
al alamin <mdabdullahalalaminp@gmail.com>
2016-10-16 20:15:48 +0600

    Added entry table for details records with model purchase, issue

--- PCL/PCL/settings.py
+++ PCL/PCL/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'master_table',
+    'entry_table',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ PCL/entry_table/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from master_table.models import Suplier, RawItem
+from django.utils.timezone import now
+
+
+class Purchase(models.Model):
+
+    raw_item = models.ForeignKey(RawItem)
+    suplier = models.ForeignKey(Suplier)
+    unit_price = models.FloatField()
+    unit_amount = models.FloatField()
+    invoice_no = models.CharField(max_length=100)
+    comment = models.TextField()
+
+    # This timefield is added just to keep track of supliers ie log them
+    creation_time = models.DateTimeField(default=now, editable=False)
+    edit_time = models.DateTimeField(default=now, editable=False)
+
+    def __str__(self):
+        return self.raw_item.name
+
+class Issue(models.Model):
+
+    raw_item = models.ForeignKey(RawItem)
+    unit_amount = models.FloatField()
+    invoice_no = models.CharField(max_length=100)
+    comment = models.TextField()
+
+    # This timefield is added just to keep track of supliers ie log them
+    creation_time = models.DateTimeField(default=now, editable=False)
+    edit_time = models.DateTimeField(default=now, editable=False)
+
+    def __str__(self):
+        return self.raw_item.name
