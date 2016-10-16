3052ce2f450128ef069625fe62a42f714f59837d
al alamin <mdabdullahalalaminp@gmail.com>
2016-10-16 19:09:19 +0600

    Added admin panel for supplier

--- PCL/master_table/admin.py
+++ PCL/master_table/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from master_table.models import Suplier
+
+
+class Suplier_Admin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'edit_time')
+    list_display_links = ('id', 'name',)
+    list_filter = ('creation_time', 'edit_time',)
+    search_fields = ('name',)
+    fieldsets = [
+        (
+            'Name Of The Supplier: ', {'fields': ['name']}
+        ),
+        (
+            'Address Of The Supplier: ', {'fields': ['address']}
+        ),
+        (
+            'Phone Numbers:', {
+                'fields': ['phone1', 'phone2', 'phone3', 'phone4', 'phone5']}
+        ),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+
+
+admin.site.register(Suplier, Suplier_Admin)
+# admin.site.register(Suplier)
--- PCL/master_table/models.py
+++ PCL/master_table/models.py
@@ -6,17 +6,14 @@ from django.utils.timezone import now
 class Suplier(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
-    phone1 = models.CharField(max_length=30)
-    phone2 = models.CharField(max_length=30)
-    phone3 = models.CharField(max_length=30)
-    phone4 = models.CharField(max_length=30)
-    phone5 = models.CharField(max_length=30)
+    phone1 = models.CharField(max_length=30, blank=True, null=True)
+    phone2 = models.CharField(max_length=30, blank=True, null=True)
+    phone3 = models.CharField(max_length=30, blank=True, null=True)
+    phone4 = models.CharField(max_length=30, blank=True, null=True)
+    phone5 = models.CharField(max_length=30, blank=True, null=True)
     # This timefield is added just to keep track of supliers ie log them
     creation_time = models.DateTimeField(default=now, editable=False)
     edit_time = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        edit_time = now
